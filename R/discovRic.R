67049ad0bd47a9a082812206d1f3a08bf38a6e61
Nachev <vladislav.nachev@charite.de>
2024-08-20 13:59:58 +0200

    report

--- R/discovRic.R
+++ R/discovRic.R
@@ -6,6 +6,7 @@ library(rcrossref)
 library(easyRPubMed)
 library(rentrez)
 library(ggVennDiagram)
+library(janitor)
 
 if (!file.exists(here("data", "raw", "intovalue.csv"))) {
   # Function to download file to `dir` within "data" directory, if not already downloaded
@@ -46,6 +47,14 @@ res <- oa_fetch(doi = iv_dois, entity = "works", mailto = Sys.getenv("EMAIL"))
 
 oaids <- res$id
 
+res2 <- oa_fetch(pmid = only_in_pm_no_doi$cited_work, entity = "works", mailto = Sys.getenv("EMAIL"))
+
+oaids2 <- res2$id
+
+sb_res <- oa_snowball(identifier = oaids2, id_type = "original", cited_by_params = list(from_publication_date = "2025-01-01"),
+                                          mailto = Sys.getenv("EMAIL"))
+
+sb_df <- snowball2df(sb_res)
 # potential alternative pipeline
 # unclear what upper bound on identifiers is (or dois). Works supposedly faster with oaids
 # so step 1 convert doi to oaid, step two chunk oaids to right size, step 3 map function over all oaids to get citing oaids
@@ -58,7 +67,7 @@ oaids <- res$id
 # sb_df <- snowball2df(sb_res)
 
 # cited_id <- "10.1016/j.bbmt.2011.12.041"
-
+cited_id <- iv_pmids_only[1]
 get_citing_dois_from_oa <- function(cited_id, sleep = 1, mailto = Sys.getenv("EMAIL")) {
   
   if (stringr::str_detect(cited_id, "^10\\.")) {
@@ -68,11 +77,23 @@ get_citing_dois_from_oa <- function(cited_id, sleep = 1, mailto = Sys.getenv("EM
       return(NULL)
     }
     cited_doi <- cited_id
+    cited_pmid <- NA
     oaid <- oaid |> 
       dplyr::pull(id)
     
+  } else if (stringr::str_detect(cited_id, "\\d{8,}")) {
+    oaid <- openalexR::oa_fetch(pmid = cited_id, entity = "works", mailto = mailto)
+    if (rlang::is_null(oaid)) {
+      warning(glue::glue("No record for id {cited_id} found on Open Alex!"))
+      return(NULL)
+    }
+    cited_doi <- NA
+    cited_pmid <- cited_id
+    oaid <- oaid |> 
+      dplyr::pull(id)
   } else {
     cited_doi <- NA
+    cited_pmid <- NA
     oaid <- cited_id
   }
   Sys.sleep(stats::rgamma(1, sleep))
@@ -82,12 +103,13 @@ get_citing_dois_from_oa <- function(cited_id, sleep = 1, mailto = Sys.getenv("EM
   
   if (rlang::is_null(records)) {
    return(tibble::tibble(cited_by_doi = "no citations found", cited_by = "no citations found",
-                 cited_work = oaid, cited_work_doi = cited_doi))
+                 cited_work = oaid, cited_work_doi = cited_doi, cited_work_pmid = cited_pmid))
   } else {
     return(records |> 
              dplyr::select(cited_by_doi = doi, cited_by = id) |> 
              dplyr::mutate(cited_work = oaid,
-                           cited_work_doi = cited_doi))
+                           cited_work_doi = cited_doi,
+                           cited_work_pmid = cited_pmid))
   }
 }
 
@@ -95,14 +117,27 @@ get_citing_dois_from_oa <- function(cited_id, sleep = 1, mailto = Sys.getenv("EM
 iv_cites <- iv_dois |> # takes ca. 4-5 hours!!!
   map(get_citing_dois_from_oa, .progress = TRUE)
 
+iv_pmids_only <- iv_data |> 
+  filter(has_pubmed == TRUE, has_doi == FALSE) |> 
+  pull(pmid)
+
+iv_cites_pmids <- iv_pmids_only |> 
+  map(get_citing_dois_from_oa, .progress = TRUE)
 
 iv_citations <- iv_cites |> 
+  list_rbind()
+
+iv_citations_pmids <- iv_cites_pmids |> 
   list_rbind()
 
 iv_citations |> 
   saveRDS(here("data", "processed", "citations.rds"))
 
 iv_citations <- readRDS(here("data", "processed", "citations.rds"))
+
+iv_citations <- iv_citations |> 
+  mutate(cited_work_pmid = NA) |> 
+  bind_rows(iv_citations_pmids)
 
 # Cochrange systematic Reviews have a doi schema:
 # cochrane_cd_regex <- "10\\.1002\\/14651858\\.cd.*pub" 
@@ -212,11 +247,16 @@ ivcpm <- ivcpm |>
   select(-cited_work_doi) |> 
   left_join(iv_ids, by = "cited_work") |> 
   select(contains("cited_work"), contains("cited_by"), everything())
-  
-ivcpm |> 
-  write_excel_csv2(here("data", "pmid_citations.csv"))
+#   
+# ivcpm |> 
+#   write_excel_csv2(here("data", "pmid_citations.csv"))
+# 
+# ivcpm <- read_csv2(here("data", "pmid_citations.csv"))
 
-ivcpm <- read_csv2(here("data", "pmid_citations.csv"))
+ivcpm |> 
+  saveRDS(here("data", "processed", "citations_pmid.rds"))
+
+ivcpm <- readRDS(here("data", "processed", "citations_pmid.rds"))
 
 crsr <- ivcpm |> 
   filter(str_detect(journal, "Cochrane"),
@@ -299,3 +339,125 @@ ggVennDiagram(cochrane) +
   scale_fill_viridis_c(option = "cividis", guide = NULL) +
   scale_color_viridis_d(option = "cividis", guide = NULL) +
   ggtitle("IntoValue publications cited in\n a Cochrane Review (or Protocol)")
+
+
+
+#search for:
+
+pmids <- c("38768286", "38657097")
+
+doi <- "10.23970/AHRQEPCCER267"
+
+cites_pmids <- pmids |> 
+  map(get_citing_dois_from_oa, .progress = TRUE)
+
+cites_doi <- doi |> 
+  map(get_citing_dois_from_oa, .progress = TRUE)
+
+resp <- oa_fetch(
+  entity = "works",
+  doi = doi,
+  # options = list(select = c("doi", "id", "cited_by_count", "type")),
+  verbose = TRUE
+)
+
+#not on openAlex, no citing articles found on PubMed
+
+cites_pm <- pmids |> 
+  map(get_citing_pmids_from_pubmed, .progress = TRUE) |> 
+  list_rbind()
+
+res <- tibble(pmid = pmids) |> 
+  get_metadata(idcol = pmid, api_key = Sys.getenv("NCBI_KEY"))
+
+res <- id_to_meta(pmids[1], api_key = Sys.getenv("NCBI_KEY"))
+
+
+ids <- pmids
+
+if (stringr::str_detect(ids[1], "^10\\.")) {
+  id_type = "[DOI]"
+} else {
+  id_type = "[UID]"
+}
+searchQuery <- purrr::reduce(paste0(ids, id_type), paste, 
+                             sep = " OR ")
+search_by_id <- get_pubmed_ids(searchQuery, api_key = api_key)
+
+res <- fetch_pubmed_data(search_by_id)
+
+res2 <- article_to_df(res)
+
+
+# 126 has_doi, but here 129
+pub_dois <- publications |> 
+  filter(has_doi == TRUE, has_pubmed == FALSE) |> 
+  distinct(doi) |> 
+  pull(doi)
+
+publications |> 
+  filter(has_doi == TRUE, has_pubmed == TRUE) |> 
+  distinct(id)
+
+id_dupes <- publications |> 
+  get_dupes(id)
+
+pmid_dupes <- publications |> 
+  get_dupes(pmid)
+
+
+doi_dupes <- publications |> 
+  get_dupes(doi)
+
+# n = 2374 dois and pmids together for both pubs and cites, so why other numbers in 
+has_both_ids_pubs <- publications |> 
+  filter(has_doi == TRUE, has_pubmed == TRUE) |> 
+  distinct(doi, pmid, .keep_all = TRUE)
+
+has_cochrane |> 
+  filter(has_doi == TRUE, has_pubmed == FALSE)
+
+has_both_ids_cites <- has_cochrane |> 
+  filter(has_doi == TRUE, has_pubmed == TRUE) |> 
+  distinct(cited_work_doi, cited_work_pmid, .keep_all = TRUE)
+
+dupes <- has_cochrane |> 
+  filter(has_doi == TRUE, has_pubmed == TRUE) |> 
+  get_dupes(cited_work_doi, cited_work_pmid)
+
+
+
+cochr_dupes <- has_cochrane |> 
+  get_dupes(cited_work)
+
+cochr_id_dupes <- has_cochrane |> 
+  get_dupes(id)
+
+
+### These 10 are not on openAlex
+dois_not_in_oa <- publications |> 
+  filter(!doi %in% has_cochrane$cited_work_doi)
+
+
+
+iv_missed_doi_cites <- dois_not_in_final$doi |> # takes ca. 4-5 hours!!!
+  map(get_citing_dois_from_oa, .progress = TRUE)
+
+iv_missed_citations <- iv_missed_doi_cites |> 
+  list_rbind()
+
+iv_citations <- iv_citations |> 
+  bind_rows(iv_missed_citations)
+
+neither <- publications |> 
+  filter(is.na(doi) & is.na(pmid)) |> 
+  select(id, url, publication_date, everything())
+
+neither |> 
+  write_excel_csv2(here("data", "processed", "missing_pmids_dois.csv"))
+
+publications |> 
+  # distinct(id, .keep_all = TRUE) |>
+  count(is.na(doi), has_doi, is.na(pmid), has_pubmed)
+
+
